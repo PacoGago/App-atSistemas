f9156f89d6648a05f73308f315bbfcaaf693aced
Gago <contacto@franciscogago.com>
2016-08-23 20:08:48 +0200

    Model
    Model de User (Usuario de la biblioteca)

---
+++ library/src/main/java/com/at/library/model/User.java
@@ -0,0 +1,70 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 4877910540692673267L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String dni;
+
+	private String name;
+
+	private String lastname;
+	
+	@Temporal(TemporalType.DATE)
+	private Date startDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+}
