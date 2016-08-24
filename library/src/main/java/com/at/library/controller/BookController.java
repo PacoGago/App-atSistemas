d88517e1d8d0b83d9e3cbe2dedc321293dd1a563
Gago <contacto@franciscogago.com>
2016-08-24 19:01:31 +0200

    Book Controller
    Ya tenemos el controlador del Book con RestController. Parte de nuestra
    Rest-API de la aplicación esta creada y probada en atSistemas.

--- library/src/main/java/com/at/library/controller/BookController.java
+++ library/src/main/java/com/at/library/controller/BookController.java
@@ -2,7 +2,11 @@ package com.at.library.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +15,43 @@ import com.at.library.dto.BookDTO;
 import com.at.library.service.book.BookService;
 
 @RestController
+@RequestMapping(value = "/book")
 public class BookController {
 
 	@Autowired
 	private BookService bookservice;
-
-	@RequestMapping(value = "/book", method = { RequestMethod.GET })
+	private static final Logger log = LoggerFactory.getLogger(BookController.class);
+	
+	@RequestMapping(method = { RequestMethod.GET })
 	public List<BookDTO> getAll() {
+		log.debug(String.format("Mostramos todos los libros."));
 		return bookservice.findAll();
 	}
-
+	
+	//TODO: Crear
+	@RequestMapping(method = { RequestMethod.POST })
+	public BookDTO create(@RequestBody BookDTO book){
+		log.debug(String.format("Vamo a crear el libro siguiente: %s", book));
+		return bookservice.create(book);
+	}
+	//TODO: Recuperar uno
+	@RequestMapping(value = "/{id}", method = {RequestMethod.POST})
+	public BookDTO findOne(@PathVariable("id") Integer id){
+		log.debug(String.format("Recuperamos un libro por id: %s", id));
+		return bookservice.findById(id);
+	}
+	
+	//TODO: Modificar
+	@RequestMapping(value = "/{id}", method = {RequestMethod.PUT})
+	public void update(@RequestBody BookDTO book){
+		log.debug(String.format("Modificamo el libro."));
+		bookservice.update(book);
+	}
+	
+	//TODO: Borrar
+	@RequestMapping(value = "/{id}", method = {RequestMethod.DELETE})
+	public void delete(@PathVariable("id") Integer id){
+		log.debug(String.format("Eliminamos un libro por su id: %s", id));
+		bookservice.delete(id);
+	}
 }
